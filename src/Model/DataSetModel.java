2497c875db3a661088d7aab8a270af5531963696
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-08-20 22:53:22 +0200

    I am thinking to move towards a better approach. Here I have modified the idl file and now it includes the DataSet for the Country and Capital, then in the model I created the model for the dataset. So now instead of adding the String in the hashmap it actually adds the object of the type dataset.

---
+++ src/Model/DataSetModel.java
@@ -0,0 +1,19 @@
+package Model;
+
+import GraphCore.DataSet;
+
+public class DataSetModel extends DataSet{	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public DataSetModel() {
+		
+	}
+	public DataSetModel(String country, String capital) {
+		this.country=country;
+		this.capital=capital;		
+		
+	}
+
+}
--- src/Server/Servant.java
+++ src/Server/Servant.java
@@ -3,14 +3,15 @@ package Server;
 import java.util.HashMap;
 
 import GraphCore.GraphCoreTestPOA;
+import Model.DataSetModel;
 
 public class Servant extends GraphCoreTestPOA {
-	private HashMap<String, String> cC = new HashMap<>();
+	private HashMap<String, DataSetModel> cC = new HashMap<>();
 
 	@Override
 	public String putValues(String country, String capital) {
 		String done="Done";
-		cC.put(country, capital);
+		cC.put(country, new DataSetModel(country,capital));
 		//System.out.println("Entered the hashMap"+country+ " And " +capital);
 		return done;
 	}
@@ -19,8 +20,8 @@ public class Servant extends GraphCoreTestPOA {
 	public String getCapital(String country) {
 		String outPut;
 		if(cC.containsKey(country)) {
-		outPut=cC.get(country);
-		//System.out.println("returning the Capital" +outPut);
+		outPut=cC.get(country).capital;
+		System.out.println("returning the Capital" +outPut);
 		return outPut;
 		}
 		String result="not found";
