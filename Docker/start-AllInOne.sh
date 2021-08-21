0d2c553d13f3cec026fa5ecf36bb4c0381c8a75c
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-08-22 01:21:25 +0200

    Docker*AllInOne* means war, jar and orbd running in one container

---
+++ Docker/start-AllInOne.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+orbd -ORBInitialPort 900  -ORBInitialHost 0.0.0.0 &
+echo "ORBD started on localhost and 900"
+java  -jar /usr/local/tomcat/webapps/graphcore-Server.jar -ORBInitialPort 900 &
+catalina.sh run
+echo "started the server"
+
+
