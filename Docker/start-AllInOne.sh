3651562681ab821697df0733234350e3f88c0255
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-08-22 04:33:27 +0200

    orbd service is not reliable for some reason

--- Docker/start-AllInOne.sh
+++ Docker/start-AllInOne.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-orbd -ORBInitialPort 900  -ORBInitialHost 0.0.0.0 &
+orbd -ORBInitialHost 0.0.0.0 -ORBInitialPort 900 &
+sleep 10
 echo "ORBD started on localhost and 900"
 java  -jar /usr/local/tomcat/webapps/graphcore-Server.jar -ORBInitialPort 900 &
-catalina.sh run
+catalina.sh run 
 echo "started the server"
 
 
--- Docker/start-api.sh
+++ Docker/start-api.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 orbd -ORBInitialPort 900 -ORBInitialHost 0.0.0.0 &
+sleep 10
 catalina.sh run
