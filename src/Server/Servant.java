993f5437431154780d425585720b57dba364b7ee
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-08-28 02:14:27 +0200

    New branch, working with hibernate. Trying to save the data in mysql

--- src/Server/Servant.java
+++ src/Server/Servant.java
@@ -1,6 +1,15 @@
 package Server;
 
 import java.util.HashMap;
+// for hibernate
+import org.hibernate.Session;    
+import org.hibernate.SessionFactory;    
+import org.hibernate.Transaction;  
+import org.hibernate.boot.Metadata;  
+import org.hibernate.boot.MetadataSources;  
+import org.hibernate.boot.registry.StandardServiceRegistry;  
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;  
+
 
 import GraphCore.GraphCoreTestPOA;
 import Model.DataSetModel;
@@ -11,7 +20,20 @@ public class Servant extends GraphCoreTestPOA {
 	@Override
 	public String putValues(String country, String capital) {
 		String done="Done";
-		cC.put(country, new DataSetModel(country,capital));
+		DataSetModel dataSetModel =new DataSetModel(country,capital);
+		cC.put(country, dataSetModel);
+		
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();  
+        Session session = factory.openSession();  
+        Transaction t = session.beginTransaction(); 
+        session.save(dataSetModel); 
+        t.commit();
+        System.out.println("Stored in MySQL");    
+        factory.close();  
+        session.close();  
+        
 		//System.out.println("Entered the hashMap"+country+ " And " +capital);
 		return done;
 	}
